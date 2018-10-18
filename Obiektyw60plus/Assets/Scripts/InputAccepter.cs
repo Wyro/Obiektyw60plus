85e23b32b3fcc42695a3b6a32be13d36b4c423ae
LGBSPL\twyrobek <tomasz.wyrobek@hotmail.com>
2018-10-18 09:09:55 +0200

    Change camera to OVRCameraRig

---
+++ Obiektyw60plus/Assets/Scripts/InputAccepter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputAccepter : MonoBehaviour
+{
+    public Rigidbody cameraRigidbody;
+
+    private void Awake()
+    {
+        PlayerInput.onTriggerDown += TriggerDown;
+        PlayerInput.onTriggerUp += TriggerUp;
+    }
+
+    private void TriggerDown()
+    {
+        print("Trigger Down!");
+        cameraRigidbody.AddForce(0, 0, 1000 * Time.deltaTime);
+    }
+
+    private void TriggerUp()
+    {
+
+    }
+}
