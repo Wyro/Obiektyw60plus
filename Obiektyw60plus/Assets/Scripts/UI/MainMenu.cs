dd93e7d1f57cd9a4391d5276220b5cd6c6221464
Aleksandra Kotula <o.k.nameless@gmail.com>
2018-11-07 23:37:21 +0100

    Added main menu functionality

---
+++ Obiektyw60plus/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject mainPanel;
+    public GameObject optionsPanel;
+    public GameObject aboutPanel;
+
+	// Use this for initialization
+	void Start () {
+        mainPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+        aboutPanel.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Exit button functionality
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    // Scene loader
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Back to main panel
+    public void BackToMainPanel()
+    {
+        mainPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+        aboutPanel.SetActive(false);
+    }
+
+    // 
+    public void SwitchPanelsToAbout()
+    {
+        mainPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        aboutPanel.SetActive(true);
+    }
+
+    // 
+    public void SwitchPanelsToOptions()
+    {
+        mainPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+        aboutPanel.SetActive(false);
+    }
+
+
+}
