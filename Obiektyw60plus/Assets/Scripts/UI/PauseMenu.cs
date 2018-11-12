4a901647a9091009a61de8bb8fda8324bdf279a7
Aleksandra Kotula <o.k.nameless@gmail.com>
2018-11-12 22:42:13 +0100

    Menu pauzy
    to do: przypiąc jakoś do gracza

---
+++ Obiektyw60plus/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject pausePanel;
+
+	// Use this for initialization
+	void Start () {
+        pausePanel.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Pause game
+        if (OVRInput.Get(OVRInput.Button.Two))
+        {
+            if (!pausePanel.activeInHierarchy)
+            {
+                PauseGame();
+            }
+            if (pausePanel.activeInHierarchy)
+            {
+                ContinueGame();
+            }
+        }
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+        //Disable scripts that still work while timescale is set to 0
+    }
+    private void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        //enable the scripts again
+    }
+
+}
