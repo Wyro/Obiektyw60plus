a9a412574158742403cfe6121f70f042a4950616
Aleksandra Kotula <o.k.nameless@gmail.com>
2018-11-15 11:25:52 +0100

    Cursed Eye Effects
    Another attempt to add them to project,

---
+++ Obiektyw60plus/Assets/Scripts/Postprocessing/TransformationEffect.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Assets.Scripts.EyeEffectsPostprocessing {
+    [ExecuteInEditMode]
+    public class TransformationEffect : MonoBehaviour
+    {
+        public Shader transfShader;
+
+        [NonSerialized]
+        Material material = null;
+
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (material == null)
+            {
+                material = new Material(transfShader);
+                transfShader.hideFlags = HideFlags.HideAndDontSave;
+            }
+
+            GetComponent<Renderer>().sharedMaterial.SetMatrix("_Trafo0",
+                this.GetComponent<Camera>().GetComponent<Renderer>().localToWorldMatrix);  
+                   
+            Graphics.Blit(source, destination, material);
+        }
+
+
+    }
+}
