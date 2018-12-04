72177cd3547542bca8e90d1bcdaa1862299afd71
Aleksandra Kotula <o.k.nameless@gmail.com>
2018-12-04 18:39:52 +0100

    Menu functionality, localization
    Fixed issue with reloading text after language change.

--- Obiektyw60plus/Assets/Scripts/Lang/LocalizedText.cs
+++ Obiektyw60plus/Assets/Scripts/Lang/LocalizedText.cs
@@ -6,9 +6,15 @@ using UnityEngine.UI;
 public class LocalizedText : MonoBehaviour {
 
     public string key;
-	// Use this for initialization
-	void Start () {
-        Text text = GetComponent<Text>();
+    Text text;
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
         text.text = LocalizationManager.instance.GetLocalizedValue(key);
-	}
+    }
+    public void ReloadText()
+    {
+        text.text = LocalizationManager.instance.GetLocalizedValue(key);
+    }
 }
