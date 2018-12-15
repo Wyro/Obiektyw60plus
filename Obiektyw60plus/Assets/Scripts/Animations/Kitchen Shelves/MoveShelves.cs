1a77460bb25399dea8d28f888ac39db981fd4c9e
xiens <xiens2@gmail.com>
2018-12-15 14:22:12 +0100

    Update MoveShelves.cs

--- Obiektyw60plus/Assets/Scripts/Animations/Kitchen Shelves/MoveShelves.cs
+++ Obiektyw60plus/Assets/Scripts/Animations/Kitchen Shelves/MoveShelves.cs
@@ -9,11 +9,16 @@ public class MoveShelves : MonoBehaviour {
     private enum MoveDirection { left, right, up, down};
     private int[] MoveDirections;
     private int[] CurrentShelvesPositions;
-    GameObject KitchenShelf;
-    
+    private GameObject KitchenShelf;
 
-	// Use this for initialization
-	void Start ()
+    private Vector3 Up = new Vector3(0, 0.73f, 0);
+    private Vector3 Down = new Vector3(0, -0.73f, 0);
+    private Vector3 Right = new Vector3(0.7f, 0, 0);
+    private Vector3 Left = new Vector3(-0.7f, 0, 0);
+
+
+    // Use this for initialization
+    void Start ()
     {
 
         AssignMoveDirections();
@@ -44,47 +49,84 @@ public class MoveShelves : MonoBehaviour {
         if (Input.GetKeyDown("x"))
         {
             //move shelves one position
-            //assign new values to CurrentPositions array
-            //going right (for now one step only)
-            //TODO adjust this to more than one step, after it is working for one step
-            for (int j = 0; j < ShelvesNum - 1; j++)
-            {
-                CurrentShelvesPositions[j] = MoveDirections[j + 1];
-            }
-            CurrentShelvesPositions[ShelvesNum - 1] = MoveDirections[0];
 
             int i = 0;
-            Vector3 DirectionToGo = transform.position + new Vector3(0.07f, 0, 0);
             foreach (Transform Shelf in KitchenShelf.transform)
             {
                 //Assign the direction to go depending on which shelf we are on
                 if(CurrentShelvesPositions[i] == (int)MoveDirection.up)
                 {
-                    DirectionToGo = Shelf.transform.position + new Vector3(0, 0, 0.08f);
+                    StartCoroutine(MoveShelfUp(Shelf, Up, Shelf.transform.position + Up));
                 }
                 else if(CurrentShelvesPositions[i] == (int)MoveDirection.down)
                 {
-                    DirectionToGo = Shelf.transform.position + new Vector3(0, 0, -0.08f);
+                    StartCoroutine(MoveShelfDown(Shelf, Down, Shelf.transform.position + Down));
                 }
                 else if (CurrentShelvesPositions[i] == (int)MoveDirection.right)
                 {
-                    DirectionToGo = Shelf.transform.position + new Vector3(0.07f, 0, 0);
+                    StartCoroutine(MoveShelfRight(Shelf, Right, Shelf.transform.position + Right));
                 }
                 else if (CurrentShelvesPositions[i] == (int)MoveDirection.left)
                 {
-                    DirectionToGo = Shelf.transform.position + new Vector3(-0.07f, 0, 0);
+                    StartCoroutine(MoveShelfLeft(Shelf, Left, Shelf.transform.position + Left));
                 }
 
-                //Move shelf in this direction
-                //TODO start coroutine for each shelf, where the shelf will be moving to its destination
-                Shelf.GetComponent<Rigidbody>().MovePosition(DirectionToGo);
                 i++;
             }
+
+            //assign new values to CurrentPositions array
+            //going right (for now one step only)
+            //TODO adjust this to more than one step, after it is working for one step
+            int temp = CurrentShelvesPositions[0];
+            for (int j = 0; j < ShelvesNum - 1; j++)
+            {
+                CurrentShelvesPositions[j] = CurrentShelvesPositions[j + 1];
+            }
+            CurrentShelvesPositions[ShelvesNum - 1] = temp;
         }
 
-
+        //TODO stop the coroutines some day
 	}
 
+    IEnumerator MoveShelfLeft(Transform Shelf, Vector3 Direction, Vector3 Destination)
+    {
+        while (Shelf.transform.position.x > Destination.x )
+        {
+            Shelf.GetComponent<Rigidbody>().MovePosition(Shelf.transform.position + Direction * Time.deltaTime);
+
+            yield return new WaitForSeconds(0.005f);
+        }
+    }
+
+    IEnumerator MoveShelfRight(Transform Shelf, Vector3 Direction, Vector3 Destination)
+    {
+        while (Shelf.transform.position.x < Destination.x)
+        {
+            Shelf.GetComponent<Rigidbody>().MovePosition(Shelf.transform.position + Direction * Time.deltaTime);
+
+            yield return new WaitForSeconds(0.005f);
+        }
+    }
+
+    IEnumerator MoveShelfUp(Transform Shelf, Vector3 Direction, Vector3 Destination)
+    {
+        while (Shelf.transform.position.y < Destination.y)
+        {
+            Shelf.GetComponent<Rigidbody>().MovePosition(Shelf.transform.position + Direction * Time.deltaTime);
+
+            yield return new WaitForSeconds(0.005f);
+        }
+    }
+
+    IEnumerator MoveShelfDown(Transform Shelf, Vector3 Direction, Vector3 Destination)
+    {
+        while (Shelf.transform.position.y > Destination.y)
+        {
+            Shelf.GetComponent<Rigidbody>().MovePosition(Shelf.transform.position + Direction * Time.deltaTime);
+
+            yield return new WaitForSeconds(0.005f);
+        }
+    }
 
 
 
