cf912ed4755285b4091e45176af02eddcd2d7abe
xiens <xiens2@gmail.com>
2018-12-18 16:33:03 +0100

    Revert "reverted to moving shelves commit"
    This reverts commit b99cf6ce241341687b668c160d3eee6629587840, reversing
    changes made to b29b52f21d075530d08062581349265874202bab.

--- Obiektyw60plus/Assets/Scripts/Spells/SpellManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SpellManager : MonoBehaviour {
-
-    public List<Spell> spellList = new List<Spell>();
-	
-}
