ba60a809b4a3274f07a959bcf3bea69a62cd2d14
Aleksandra Kotula <o.k.nameless@gmail.com>
2018-12-30 00:25:45 +0100

    Architect Panel
    Work in progress

---
+++ Obiektyw60plus/Assets/Scripts/UI/ArchitectMenu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArchitectMenu : MonoBehaviour {
+
+    public Toggle wheelchairToggle;
+    public GameObject wheelchairMesh;
+    public GameObject eyesCamera;
+
+	// Use this for initialization
+	void Start () {
+        wheelchairToggle.onValueChanged.AddListener(delegate {
+            wheelchairToggleChanged(wheelchairToggle);
+        });
+
+    }
+
+    private void wheelchairToggleChanged(Toggle wheelchairToggle)
+    {
+        if (wheelchairToggle.isOn)
+        {
+            wheelchairMesh.SetActive(true);
+            eyesCamera.transform.Translate(new Vector3(0f, -0.5f, 0f));
+        }
+        else
+        {
+            wheelchairMesh.SetActive(false);
+            eyesCamera.transform.Translate(new Vector3(0f, 0.5f, 0f));
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
